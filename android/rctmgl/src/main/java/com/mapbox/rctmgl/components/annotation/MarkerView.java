be55759c30c4ae391bce5ae7e9d7b40e0a443d0d
Matei <matei.soare0@gmail.com>
2021-05-22 14:17:36 +0200

    Fix: Markerview on Android not moving when swiping on the map

--- android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/MarkerView.java
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/MarkerView.java
@@ -1,10 +1,12 @@
 package com.mapbox.rctmgl.components.annotation;
 
+import android.graphics.PointF;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.Projection;
 
 /**
  * Subclass of MarkerView so we MarkerViewManager can implement remove/restoreViews
@@ -12,12 +14,35 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 public class MarkerView extends com.mapbox.mapboxsdk.plugins.markerview.MarkerView {
     View view;
 
+    private LatLng latLng;
+    private Projection projection;
+    private OnPositionUpdateListener onPositionUpdateListener;
+
     public MarkerView(@NonNull LatLng latLng, @NonNull View view) {
         super(latLng, view);
         this.view = view;
+        this.latLng = latLng;
     }
 
     public View getView() {
         return this.view;
+    }
+
+    public void update() {
+        PointF point = projection.toScreenLocation(latLng);
+        if (onPositionUpdateListener != null) {
+            point = onPositionUpdateListener.onUpdate(point);
+        }
+        view.setX(point.x);
+        view.setY(point.y);
+    }
+
+    @Override
+    public void setOnPositionUpdateListener(OnPositionUpdateListener onPositionUpdateListener) {
+        this.onPositionUpdateListener = onPositionUpdateListener;
+    }
+
+    public void setProjection(Projection projection) {
+        this.projection = projection;
     }
 }
--- android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/MarkerViewManager.java
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/MarkerViewManager.java
@@ -1,9 +1,14 @@
 package com.mapbox.rctmgl.components.annotation;
 
+import android.graphics.PointF;
+
 import androidx.annotation.NonNull;
 
+import com.mapbox.android.gestures.MoveGestureDetector;
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.rctmgl.components.annotation.MarkerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +19,24 @@ import java.util.List;
 public class MarkerViewManager extends com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager {
     private final List<MarkerView> markers = new ArrayList<>();
     private MapView mapView;
+    private MapboxMap mapboxMap;
 
     public MarkerViewManager(MapView mapView, MapboxMap mapboxMap) {
         super(mapView, mapboxMap);
         this.mapView = mapView;
-        // this.mapboxMap = mapboxMap;
+        this.mapboxMap = mapboxMap;
+        this.mapboxMap.addOnCameraMoveListener(new MapboxMap.OnCameraMoveListener() {
+             @Override
+             public void onCameraMove() {
+                 update();
+             }
+         });
     }
 
     public void addMarker(@NonNull MarkerView markerView) {
         super.addMarker(markerView);
         markers.add(markerView);
+        markerView.setProjection(mapboxMap.getProjection());
     }
 
     public void removeMarker(@NonNull MarkerView markerView) {
@@ -40,6 +53,12 @@ public class MarkerViewManager extends com.mapbox.mapboxsdk.plugins.markerview.M
     public void restoreViews() {
         for (MarkerView marker: markers) {
             mapView.addView(marker.getView());
+        }
+    }
+
+    private void update() {
+        for (MarkerView marker : markers) {
+            marker.update();
         }
     }
 }
